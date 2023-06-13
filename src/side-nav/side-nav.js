fcaae698cf29cbe9d4fbfbb9bca41e62aa6608d5
Akashiutchiha <christianhonore2003@gmail.com>
2023-06-13 19:20:10 +0100

    fix: Fixing the UI

--- src/side-nav/side-nav.js
+++ src/side-nav/side-nav.js
@@ -8,7 +8,6 @@ const SideNav = () => {
                 <i className="bi bi-person"></i>
                 <i className="bi bi-soundwave"></i>
                 <i className="bi bi-inbox"></i>
-
                 <i className="bi bi-chat-dots"></i>
                 <i className="bi bi-file-earmark"></i>
                 <i className="bi bi-speaker-fill"></i>
